bc900c1d9e3bb4906f1b5d9b4b2394ca9a1b57e5
MaybemarHernandez <maybemar.hernandez@rheonics.com>
2024-08-13 10:17:56 -0500

    Add the spotify client ID and secret

--- Main.py
+++ Main.py
@@ -2,29 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
+from Keys import CLIENT_ID, SPOTIFY_SECRET
 
+#define constants
 BILLBOARD_URL = 'https://www.billboard.com/charts/hot-100/'
-SPOTIFY_CLIENT_ID = '596bf299646e45d6907c4ec604f08e3d'
-SPOTIFY_SECRET = '5534d23bbce047dfa90e4301a129778e'
 
+#query the user for the date and request the website information
 users_date = input("Which year would you like to travel to? Type the date in this format YYYY-MM-DD: ")
 website_info = requests.get(url=BILLBOARD_URL + users_date + '/')
 
+#scrape the site with beautiful soup
 soup = BeautifulSoup(website_info.text, 'html.parser')
 song_name_spans = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_name_spans]
 
-#print(song_names)
-
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIENT_ID,
+#login to spotify with spotipy
+sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=CLIENT_ID,
                                                scope="playlist-modify-private",
                                                client_secret=SPOTIFY_SECRET,
                                                redirect_uri="https://example.com/callback",
                                                ))
-
+#read and save the user id from the authentication
 user_id = sp.current_user()["id"]
-print(user_id)
 
+#search the songs by uri
 year = users_date.split("-")[0]
 song_uris = []
 for song in song_names:
@@ -35,6 +36,7 @@ for song in song_names:
     except IndexError:
         print(f"{song} doesn't exist in Spotify. Skipped.")
 
+#cretae the new Playlist with the URIs found
 playlist = sp.user_playlist_create(user_id,f"100 songs of year {year}", public=False, collaborative=False, description=f"Top Billboard 100 songs of year {year}")
 print(playlist)
 sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
